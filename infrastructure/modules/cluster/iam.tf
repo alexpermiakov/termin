93855b497b169d67be9269cf35d3a97f0d0e7cc8
Alex <alex.permyakoff@gmail.com>
2025-03-24 21:12:58 +0100

    make sub a string

--- infrastructure/modules/cluster/iam.tf
+++ infrastructure/modules/cluster/iam.tf
@@ -15,10 +15,7 @@ resource "aws_iam_role" "terraform_execution_role" {
           "token.actions.githubusercontent.com:iss" = "https://token.actions.githubusercontent.com"
         },
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = [
-            "repo:alexpermiakov/termin:environment:staging",
-            "repo:alexpermiakov/termin:pull_request"
-          ]
+          "token.actions.githubusercontent.com:sub" = "repo:alexpermiakov/termin:environment:staging"
         }
       }
     }]
